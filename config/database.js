da6c8837d360687ddd75977beaaab7f6fd870d0d
Bruno Casotto <BrunoCasotto@QuickTechs-MacBook-Pro-2.local>
2017-01-07 00:47:31 -0200

    Config changed to env

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 module.exports  = {
-    apiKey: "AIzaSyCKAZF2oajMvnEfgOJwBO__PPqZ3nUgOkA",
-    authDomain: "blog-9d0bc.firebaseapp.com",
-    databaseURL: "https://blog-9d0bc.firebaseio.com",
-    storageBucket: "blog-9d0bc.appspot.com",
-    messagingSenderId: "642355234164"
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    databaseURL: process.env.DATABASEURL,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId: process.env.MESSAGINGSENDERID
   }
