1b96bd57387e91967a794f0991fe311720a4342a
Bruno Casotto <casottoalves@hotmail.com>
2017-02-17 14:04:07 -0200

    dashboard refactoring initial

--- resources/assets/js/common/components/Menu.vue
+++ resources/assets/js/common/components/Menu.vue
@@ -1,30 +1,55 @@
 <template lang="html">
 	<div class="menu">
 		<div class="items">
-			<div class="item">Home</div>
-			<div class="item">Cursos</div>
-			<div class="item">item2</div>
-			<div class="item">item3</div>
-			<div class="item">Configurações</div>
+			<div class="item item--hidden"><span class="fa fa-arrow-left">Esconder</span></div>
+			<div class="item"><span class="fa fa-users">Turmas</span></div>
+			<div class="item"><span class="fa fa-address-card">Alunos</span></div>
+			<div class="item"><span class="fa fa-pencil-square-o">Aulas</span></div>
+			<div class="item"><span class="fa fa-mortar-board">Cursos</span></div>
+			<div class="item"><span class="fa fa-cog">Configurações</span></div>
+			<div class="item"><span class="fa fa-user">Meu Perfil</span></div>
+			<div class="item"><span class="fa fa-lock">Segurança</span></div>
 		</div>
 	</div>
 </template>
 
 <script>
 	export default {
-		name: 'Menu'
+		data: function() {
+			return {
+			}
+		}
 	}
 </script>
 
 <style lang="sass">
+	@import "~_config/_vars.scss";
 
 	.menu {
 		.items {
-			display: flex;
-
+			width: 100%;
 
 			.item {
+				display: flex;
+				height: 45px;
+				border-bottom: solid 1px $color-grey--light;
+				padding: 0 10px;
+				cursor: pointer;
 
+				&--hidden {
+					span {
+						margin-left: auto;
+					}
+				}
+
+				span {
+					line-height: 45px;
+
+					&:before {
+						margin-right: 5px;
+						font-size: 1.3rem;
+					}
+				}
 			}
 		}
 	}
--- resources/assets/js/components/pages/Dashboard.vue
+++ resources/assets/js/components/pages/Dashboard.vue
@@ -2,17 +2,27 @@
    	<div class="dashboard">
 		<div class="dashboard__header">
 		</div>
+		<div class="dashboard__content">
+			<div class="menu">
+				<side-menu></side-menu>
+			</div>
+			<div class="content">
+
+			</div>
+		</div>
 	</div>
 </template>
 <script>
     import VHeader from "_components/includes/Header.vue"
 	import VFooter from "_components/includes/Footer.vue"
-	
+	import SideMenu from "_common/components/Menu.vue"
+
     export default {
         name: 'Dashboard',
         components: {
             VHeader,
-            VFooter
+            VFooter,
+			SideMenu
         },
 		data () {
 			return{
@@ -26,9 +36,28 @@
 	@import "~_config/_vars.scss";
 	
 	.dashboard {
+		height: calc(100vh - 180px);
+
 		&__header {
 			border-bottom: solid 1px $color-grey--light;
 			height: 100px;
 		}
+
+		.dashboard__content {
+			display: flex;
+			padding: 0 50px;
+			height: 100%;
+			border-left: solid 0.5px $color-grey--light;
+			
+			.menu {
+				flex: 1;
+				border-left: solid 0.5px $color-grey--light;
+				border-right: solid 0.5px $color-grey--light;
+			}
+			.content {
+				flex: 4;
+			}
+		}
+
 	}
 </style>
