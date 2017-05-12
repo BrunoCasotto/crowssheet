7198424bdc38f8bb76078000f2bbe852807dee7d
Bruno Casotto <casottoalves@hotmail.com>
2017-05-12 02:00:02 -0300

    refactoring

--- resources/assets/js/components/pages/CourseRoom.vue
+++ resources/assets/js/components/pages/CourseRoom.vue
@@ -17,10 +17,14 @@
 	import axios from "axios"
 	import growl from "growl-alert"
 	import courseService from '_service/course'
-	import ClassForm from '_components/includes/form/ClassForm.vue'
 	import ClassList from '_components/includes/list/ClassList.vue'
 
 	export default {
+		data () {
+			return {
+				classList: []
+			}
+		},
 		computed: {
 			user: function () {
 				return this.$store.state.Session
@@ -29,65 +33,17 @@
 				return this.$store.state.Course
 			}
 		},
-		data () {
-			return {
-				edit: false,
-				classForm: false,
-				classList: []
-			}
-		},
 		mounted() {
 			this.filterClassList()
 		},
 		components: {
-			ClassForm,
 			ClassList
 		},
 		methods: {
-			toggleUpdate(){
-				this.edit = !this.edit
-			},
-			toggleClassForm(){
-				this.classForm = !this.classForm
-			},
 			filterClassList() {
 				for(var key in this.course.classes) {
 					this.course.classes[key]['key'] = key
 					this.classList.push(this.course.classes[key])
-				}
-			},
-			update() {
-				this.$store.dispatch('toggleLoader', true)
-				if(this.validateForm()) {
-					courseService
-					.update( this.user.uid, this.course, this.course.key )
-					.then(response => {
-						if (response.data.status) {
-							this.$store.dispatch('toggleLoader', false)
-							growl.success('Salvo')
-							this.toggleUpdate()
-						} else {
-							this.$store.dispatch('toggleLoader', false)
-							growl.error(response.data.data.message)
-						}
-					})
-					.catch(error => {
-						this.$store.dispatch('toggleLoader', false)
-						growl.error(error.data.message)
-					})
-					this.$store.dispatch('toggleLoader', false)
-				}
-			},
-			validateForm () {
-				this.course.description = tinymce.get('form-description').getContent()
-				if(this.course.title.length < 5) {
-					growl.warning('O titulo deve conter no minimo 5 caracteres')
-				} else {
-					if(this.course.description.length < 10) {
-						growl.warning('A descrição deve conter no minimo 10 caracteres') 
-					} else {
-						return true
-					}
 				}
 			}
 		}
